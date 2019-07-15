c38986df147b6f90511fe1607ef6bdcaa23f920b
Ohm Piromrak <ohmpzzz@gmail.com>
2019-07-16 04:14:37 +0700

    fix(seo): remove duplicate import `prop-types` 😂

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 function SEO({ description, lang, meta, image, title, slug }) {
