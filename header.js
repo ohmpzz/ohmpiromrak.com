d286b81a99816839ecf8a1e48138d26361fee028
Ohm Piromrak <ohmpzzz@gmail.com>
2019-10-16 21:41:46 +0700

    update: security headers

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "/(.*)": [
+  "/static/(.*)": [
     "cache-control:public, max-age=0, must-revalidate",
     "Content-Security-Policy: default-src 'self' https://www.google-analytics.com https://ajax.cloudflare.com; script-src 'self' 'unsafe-inline' https://www.google-analytics.com https://ajax.cloudflare.com; style-src 'self' data: 'unsafe-inline'; img-src 'self' data: https: blob:; font-src 'self' https: data:; object-src 'none';",
     "feature-policy: payment 'none';microphone 'none';camera 'none'",
@@ -11,6 +11,7 @@ module.exports = {
     "Access-Control-Allow-Methods: GET, OPTIONS",
     "Access-Control-Allow-Origin: https://ohmpiromrak.com, https://www.google-analytics.com, https://ajax.cloudflare.com",
   ],
+
   "/(.*\\.(js|json|css|ico|png)$)": [
     "cache-control: public,max-age=31536000,immutable",
     "Content-Security-Policy: default-src 'self' https://www.google-analytics.com https://ajax.cloudflare.com; script-src 'self' 'unsafe-inline' https://www.google-analytics.com https://ajax.cloudflare.com; style-src 'self' data: 'unsafe-inline'; img-src 'self' data: https: blob:; font-src 'self' https: data:; object-src 'none';",
@@ -23,5 +24,28 @@ module.exports = {
     "Access-Control-Allow-Methods: GET, OPTIONS",
     "Access-Control-Allow-Origin: https://ohmpiromrak.com, https://www.google-analytics.com, https://ajax.cloudflare.com",
   ],
-  "/sw.js": ["cache-control: public, max-age=0, must-revalidate"],
+  "/sw.js": [
+    "cache-control: public,max-age=31536000,immutable",
+    "Content-Security-Policy: default-src 'self' https://www.google-analytics.com https://ajax.cloudflare.com; script-src 'self' 'unsafe-inline' https://www.google-analytics.com https://ajax.cloudflare.com; style-src 'self' data: 'unsafe-inline'; img-src 'self' data: https: blob:; font-src 'self' https: data:; object-src 'none';",
+    "feature-policy: payment 'none';microphone 'none';camera 'none'",
+    "referrer-policy: no-referrer-when-downgrade",
+    "x-content-type-options: nosniff",
+    "x-download-options: noopen",
+    "x-frame-options: SAMEORIGIN",
+    "x-xss-protection: 1; mode=block",
+    "Access-Control-Allow-Methods: GET, OPTIONS",
+    "Access-Control-Allow-Origin: https://ohmpiromrak.com, https://www.google-analytics.com, https://ajax.cloudflare.com",
+  ],
+  "/(.*)": [
+    "cache-control:public, max-age=0, must-revalidate",
+    "Content-Security-Policy: default-src 'self' https://www.google-analytics.com https://ajax.cloudflare.com; script-src 'self' 'unsafe-inline' https://www.google-analytics.com https://ajax.cloudflare.com; style-src 'self' data: 'unsafe-inline'; img-src 'self' data: https: blob:; font-src 'self' https: data:; object-src 'none';",
+    "feature-policy: payment 'none';microphone 'none';camera 'none'",
+    "referrer-policy: no-referrer-when-downgrade",
+    "x-content-type-options: nosniff",
+    "x-download-options: noopen",
+    "x-frame-options: SAMEORIGIN",
+    "x-xss-protection: 1; mode=block",
+    "Access-Control-Allow-Methods: GET, OPTIONS",
+    "Access-Control-Allow-Origin: https://ohmpiromrak.com, https://www.google-analytics.com, https://ajax.cloudflare.com",
+  ],
 }
